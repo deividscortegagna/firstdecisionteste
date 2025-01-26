4258f8b19ae88e8a2425c85066f0d7cbdabe7adb
Deivid Willian Scortegagna <deividwillianrs@gmail.com>
2025-01-26 15:48:39 -0300

    feat: add user services

---
+++ app/UserService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use App\UserRepositoryInterface;
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function createUser(array $data)
+    {
+        $data['password'] = bcrypt($data['password']);
+        return $this->userRepository->create($data);
+    }
+
+    public function updateUser(int $id, array $data)
+    {
+        if (isset($data['password'])) {
+            $data['password'] = bcrypt($data['password']);
+        }
+        return $this->userRepository->update($id, $data);
+    }
+
+    public function deleteUser(int $id)
+    {
+        return $this->userRepository->delete($id);
+    }
+
+    public function findUserById(int $id)
+    {
+        return $this->userRepository->findById($id);
+    }
+}
