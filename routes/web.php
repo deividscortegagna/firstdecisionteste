3e2f6e059aade7817c7c61e581c06ea2076056ff
Deivid Willian Scortegagna <deividwillianrs@gmail.com>
2025-01-26 15:02:23 -0300

    feat: web routes config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Livewire\RegisterUser;
+use App\Livewire\LoginUser;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('register', RegisterUser::class)->name('register');
+    Route::get('login', LoginUser::class)->name('login');
+});
+
+Route::middleware('auth')->prefix('dashboard')->group(function () {
+    Route::post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
